e012ed48a2f2e7749d24dd7cf9d1fee790b908f3
wavesinaroom <jaureguij@javeriana.edu.co>
2023-04-18 15:48:44 -0500

    [Feature] Cart Component

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,6 +1,14 @@
+import CartItem from './cartItem'
+import uniqid from 'uniqid'
+
 const Cart = ({cart, removeCart})=>{
   return(
-
+    <div className="cart-items">
+      {cart.map((purchase)=>
+          <CartItem key={uniqid()} item={purchase} removeCart={()=>{removeCart()}}/>
+      )};
+    </div>
 
   );
 }
+export default Cart;
