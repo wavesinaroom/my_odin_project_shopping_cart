37d81a9149e70c840b6a60cb1605aa2b74e53c9c
wavesinaroom <jaureguij@javeriana.edu.co>
2023-04-19 16:26:03 -0500

    [Feature] Checkout button plus test

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,16 +1,33 @@
 import CartItem from './cartItem'
 import uniqid from 'uniqid'
+import {useState} from 'react';
 
 const Cart = ({cart, removeCart, isRendered})=>{
+  const [isNotified, setIsNotified] = useState(false);
+
+  const checkOut = ()=>{
+    setIsNotified(true);
+  }
+
   if(!isRendered)
     return null;
 
+
+  if(isNotified){
+    return(
+    <div className="cart-items">
+      <p>Thanks for shopping at VST store</p>
+      <button>Back</button>
+    </div>
+    );
+  }
+  
   return(
     <div className="cart-items">
       {cart.map((purchase)=>
-          <CartItem key={uniqid()} item={purchase} removeCart={()=>{removeCart()}}/>
-      )};
-      <button>Checkout</button>
+        <CartItem key={uniqid()} item={purchase} removeCart={()=>{removeCart()}}/>
+      )}
+      <button onClick={()=>{checkOut();}}>Checkout</button>
       <button>Back</button>
     </div>
   );
--- src/components/cart.test.js
+++ src/components/cart.test.js
@@ -1,34 +1,50 @@
 import React from "react";
-import {cleanup, render, screen, waitFor} from "@testing-library/react";
+import {cleanup, fireEvent, render, screen, waitFor} from "@testing-library/react";
 import  userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom';
 import {MemoryRouter, Route, Routes} from "react-router";
 import App from "../App";
+import Cart from "./cart";
+import Items from './itemsList.json'
 
 afterEach(cleanup);
 
-describe(`Cart button toggle`,()=>{
 
-    render(
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<App/>}/>
-        </Routes>
-      </MemoryRouter>);
-  it(`toggles cart panel on`,async()=>{
-    userEvent.click(screen.queryByAltText(`cart-icon`));
+render(
+  <MemoryRouter>
+    <Routes>
+      <Route path="/" element={<App/>}/>
+    </Routes>
+  </MemoryRouter>);
 
-    await waitFor(()=>{
-      expect(screen.getByRole(`button`, {name: `Checkout`})).toBeInTheDocument();
-    });
+it(`toggles cart panel on`,async()=>{
+  userEvent.click(screen.getByAltText(`cart-icon`));
+
+  await waitFor(()=>{
+    expect(screen.getByRole(`button`, {name: `Checkout`})).toBeInTheDocument();
   });
+});
 
-  it(`toggles cart panel off`,async()=>{
-    userEvent.click(screen.queryByAltText(`cart-icon`));
+it(`toggles cart panel off`,async()=>{
+  userEvent.click(screen.queryByAltText(`cart-icon`));
 
-    await waitFor(()=>{
-      expect(screen.queryByRole(`button`, {name: `Checkout`})).not.toBeInTheDocument();
-    });
+  await waitFor(()=>{
+    expect(screen.queryByRole(`button`, {name: `Checkout`})).not.toBeInTheDocument();
   });
+});
+
+
+it(`Checks out items`, async()=>{
+  const removeMock = jest.fn();
+  const renderBool = true;
+  
+  render(
+    <Cart cart={Items} removeCart={removeMock} isRendered={renderBool}/> 
+  );
+
+  fireEvent.click(screen.getByRole(`button`, {name: `Checkout`}));
+
+    expect(screen.getByText(`Thanks for shopping at VST store`)).toBeInTheDocument();
+    expect(screen.queryByRole(`button`, {name:`Checkout`})).not.toBeInTheDocument();
 });
 
