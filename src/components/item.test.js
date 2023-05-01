3a77881e655bc5bd93dea9d9c72fd92ff60e9522
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-01 16:59:43 -0500

    [Task] Perform item component tests

---
+++ src/components/item.test.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { render, cleanup,screen,  fireEvent, getByRole} from '@testing-library/react';
+import '@testing-library/jest-dom'
+import Item from './item';
+
+
+afterEach(cleanup);
+const itemMock = {pic: `a pic`, name:`a name`, description:`a description`, type:`VST`, price:0}
+const setMock = jest.fn();
+
+it(`renders content`,()=>{
+  render(<Item item={itemMock} setItem={setMock}/>);
+
+  expect(screen.getByRole(`heading`, {name: `${itemMock.name}`})).toBeInTheDocument();
+  expect(screen.getByAltText(`${itemMock.name}`)).toBeInTheDocument();
+  expect(screen.getByText(`${itemMock.description}`)).toBeInTheDocument();
+  expect(screen.getByText(`${itemMock.price}`)).toBeInTheDocument();
+})
+
+it(`handles store item hook`,()=>{
+  render(<Item item={itemMock} setItem={setMock}/>);
+
+  fireEvent.click(screen.getByAltText(`${itemMock.name}`));
+  expect(setMock).toBeCalled();
+});
