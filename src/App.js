e71c61390b3a5672eda6905e8ac7cbc718dd0d8e
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-02 17:33:16 -0500

    [Feature] Test route to store from landing page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import './App.css';
-import React, {useState} from 'react';
-import uniqid from 'uniqid'
-import Items from './components/itemsList.json' //pic,name,description,type,price
-import Item from './components/item';
-import ItemPanel from './components/itemPanel';
-import Cart from './components/cart';
-
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const App = ()=> {
-  const [isCartRendered, setIsCarRendered] = useState(false);
-  const [cart, setCart] = useState([]);
-  const [item,setItem] = useState(null);
-  const [showInfo,setShowInfo] = useState(false);
-
+    
   return (
     <>
-      <h1 data-testid='main-title'>VST shop</h1>
-      <div className='cart'>
-        <img alt='cart-icon' onClick={()=>{setIsCarRendered(!isCartRendered)}}></img>
-        <Cart setCart={setCart} cart={cart}  isRendered={isCartRendered}/>
-      </div>
-      <div data-testid='items-display' className='display'>
-        {Items.map((item)=>
-          <Item key={uniqid()} item={item} setItem={setItem}/>
-        )}
-      </div>
-      <ItemPanel item={item} setCart={setCart} show={showInfo} onClickOutside={()=>{setShowInfo(false)}}/>   
+      <h1>Welcome to the VST store</h1>
+      <Link to='/store'><button>Enter</button></Link>
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,73 +4,36 @@ import  userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom';
 import {MemoryRouter, Route, Routes} from "react-router-dom";
 import App from "./App"
-import ItemPanel from "./components/itemPanel";
+import Store from "./components/store"
 
 afterEach(cleanup);
 
-it(`Renders correctly`,()=>{
+it(`renders correctly`,()=>{
   render(
     <MemoryRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
-        <Route path='/item' element={<ItemPanel/>}/>
       </Routes>
     </MemoryRouter>
   );
-  expect(screen.getByTestId(`main-title`)).toBeInTheDocument();
-  expect(screen.getByTestId(`items-display`)).toBeInTheDocument();
+
+  expect(screen.getByRole(`heading`,{name:`Welcome to the VST store`})).toBeInTheDocument();
+  expect(screen.getByRole(`button`, {name:`Enter`})).toBeInTheDocument();
 });
 
-it(`Renders twelve items`, ()=>{
+it(`gets user to store after button click`, async()=>{
   render(
     <MemoryRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
-        <Route path="/item" element={<ItemPanel/>}/>
-      </Routes>
-    </MemoryRouter>
-  );
-  expect(screen.getAllByTestId('item-test')).toHaveLength(12);
-}); 
-
-test.skip(`toggles cart panel on/off`,async()=>{
-  render(
-    <MemoryRouter>
-      <Routes>
-        <Route path="/" element={<App/>}/>
-      </Routes>
-    </MemoryRouter>);
-
-  userEvent.click(screen.getByAltText(`cart-icon`));
-
-  await waitFor(()=>{
-    expect(screen.getByRole(`button`, {name: `Checkout`})).toBeInTheDocument();
-  });
-
-  userEvent.click(screen.queryByAltText(`cart-icon`));
-
-  await waitFor(()=>{
-    expect(screen.queryByRole(`button`, {name: `Checkout`})).not.toBeInTheDocument();
-  });
-});
-it(`Adds an item to cart`,async ()=>{
-  const changeSize = jest.fn();
-  const handleClick = jest.spyOn(React, "useState");
-  handleClick.mockImplementation(size => [size, changeSize]);
-  render(
-    <MemoryRouter>
-      <Routes>
-        <Route path='/' element={<App/>}/>
-        <Route path="/item" element={<ItemPanel/>}/>
+        <Route path='/store' element={<Store/>}/>
       </Routes>
     </MemoryRouter>
   );
 
-  userEvent.click(screen.queryByAltText(`Reaper`));
-  userEvent.click(screen.queryByRole(`button`,{name:`Add`}));
+  userEvent.click(screen.getByRole(`button`, {name:`Enter`}));
 
   await waitFor(()=>{
-    expect(changeSize).toBeCalled();
+    expect(screen.getByAltText(`Reaper`)).toBeInTheDocument();
   });
 });
-
