dcec2d5a9287e07db85afd9931740d8c2ff7820c
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-03 17:40:05 -0500

    [Task] Clean cartItem.js from unnecessary remove function

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -1,13 +1,9 @@
 
 const CartItem = ({item, setCart})=>{
 
-  function removeCart(info){
-    setCart(cart => cart.filter(item => item.name !== info.name));
-  }
-
   return(
     <div className="cart-item">
-      <button data-testid={item.name} onClick={()=>{setCart({item})}}>-</button>
+      <button data-testid={item.name} onClick={()=>setCart({item})}>-</button>
       <img src={item.pic} alt={item.name}></img>
       <h4>{item.name}</h4>
       <p>{item.price}</p>
