20abbae94ad71d15b987258614a9a9205be2d71d
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-05 09:22:57 -0500

    [Task] Layout itempanel with items display

--- src/components/store.js
+++ src/components/store.js
@@ -24,12 +24,14 @@ const Store = ()=> {
           <Cart data-testid='cart-display' setCart={target=>(remove(target))} cart={cart}/>
         </div>
       </div>
-      <div data-testid='items-display' className='display'>
-        {Items.map((item)=>
-          <Item key={uniqid()} item={item} setItem={setItem}  openPanel={()=>{setIsModalOpen(true)}}/>
-        )}
-      </div>
-      <ItemPanel item={item} setCart={setCart} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+        <div className='shop'>
+          <ItemPanel item={item} setCart={setCart} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+          <div data-testid='items-display' className='display'>
+            {Items.map((item)=>
+              <Item key={uniqid()} item={item} setItem={setItem}  openPanel={()=>{setIsModalOpen(true)}}/>
+            )}
+          </div>
+        </div>
     </div>
   );
 }
