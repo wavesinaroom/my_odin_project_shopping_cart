a155eb2d9478120d1df407bd018aacd953cd5379
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-08 17:26:24 -0500

    [Task] Come back to routes and route structure

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-        <App/>
+      <Routes>
+          <Route path='/' element={<App/>}/>
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
