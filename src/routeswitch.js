bfe836116a88d3fe950bd8aa1ea0f4040d855dfa
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-08 16:42:39 -0500

    [Task] Remove routes component

--- src/routeswitch.js
+++ src/routeswitch.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { HashRouter,Routes,Route } from "react-router-dom";
+import { HashRouter,Route } from "react-router-dom";
 import App from "./App"
 import Store from "./components/store";
 
 const RouteSwitch = () =>{
   return(
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<App/>}/>
-        <Route path="/store" element={<Store/>}/>
-      </Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="/store" element={<Store/>}/>
     </HashRouter>
   );
 }
