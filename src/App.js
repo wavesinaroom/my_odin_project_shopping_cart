0c4da0c66732dfe3f53fee28e37b3ac155e89a15
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-08 14:12:44 -0500

    [Task] Style landing page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import {Link} from 'react-router-dom';
 const App = ()=> {
     
   return (
-    <>
-      <h1>Welcome to the VST store</h1>
+    <div style={div}>
+      <h1 style={header}>Welcome to the VST store</h1>
       <Link to='/store'><button>Enter</button></Link>
-    </>
+    </div>
   );
 }
 
 export default App;
+
+const div = {
+  display:'flex',
+  flexDirection:'column',
+  position: 'absolute',
+  left: '20vw',
+  top: '50vh'
+}
+
+const header = {
+  marginBottom: '3em'
+}
