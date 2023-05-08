7e51674aea85f350fd2a01215ec10e2aacffaac7
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-08 16:37:14 -0500

    [Task] Change routeswitch into hashrouter

--- src/routeswitch.js
+++ src/routeswitch.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { HashRouter,Routes,Route } from "react-router-dom";
 import App from "./App"
 import Store from "./components/store";
 
 const RouteSwitch = () =>{
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App/>}/>
         <Route path="/store" element={<Store/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
