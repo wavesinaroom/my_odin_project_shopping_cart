9d241ab925fe3c3b3c381a8f19a433981278ab32
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-08 17:05:40 -0500

    [Task] Add routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App'
 import Store from './components/store'
@@ -9,8 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Route exact path="/" component={<App/>}/>
-      <Route exact path="/store" component={<Store/>}/>
+      <Routes>
+        <Route exact path="/" component={<App/>}/>
+        <Route exact path="/store" component={<Store/>}/>
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
