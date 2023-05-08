1e351922260b56b6a6218ec5419244cb66587409
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-08 17:15:02 -0500

    [Task] Remove routes component

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
         <App/>
-      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
